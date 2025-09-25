2de6a7be9887d0356d08f455cc75db99b5a94e7c
PalmisanoGaston <130399416+PalmisanoGaston@users.noreply.github.com>
2025-09-25 12:44:48 -0300

    Actualizacion Alumno

--- src/pages/Alumno/VirtualTour.jsx
+++ src/pages/Alumno/VirtualTour.jsx
@@ -6,7 +6,7 @@ export default function VirtualTour(){
       <div className="card">
         <p>Embed de recorrida 360 o video. Reemplaza el iframe por tu tour (Google Street View, Kuula o video privado).</p>
         <div style={{aspectRatio:'16/9'}}>
-          <iframe title="recorrida" src="https://www.youtube.com/embed/dQw4w9WgXcQ" style={{width:'100%',height:'100%'}} />
+                  <iframe title="recorrida" src="https://www.youtube.com/watch?v=S4B7r3kew-4" style={{width:'100%',height:'100%'}} />
         </div>
       </div>
     </div>
